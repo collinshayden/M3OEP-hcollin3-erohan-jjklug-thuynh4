74550760dbd1851abdc21fd09bb8daee702d0d26
rohbot25 <ewrohan@gmail.com>
2023-03-20 23:51:31 -0400

    stockfish

--- stockfish.py
+++ stockfish.py
@@ -1,11 +1,16 @@
 #imports
-
+import sys
+from stockfish import Stockfish
 #get arguments from c++
-
-#FEN
+FEN = sys.argv[1]
+elo = sys.argv[2]
 
 #put FEN into stockfish
-
+stockfish.set_elo_rating(elo)
+stockfish.set_fen_position(FEN)
 #get best move
-
+if(stockfish.is_fen_valid()):
+    move = stockfish.get_best_move()
+else:
+    move = "invalid"
 #return best move to c++
